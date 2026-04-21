e891d145ebee2bd1eab6ec294a4c5bdfe5386715
Devansh Dhingra <135154928+devanshdhingra@users.noreply.github.com>
2026-04-21 15:30:08 +0530

    Change app run configuration and remove home route

--- app.py
+++ app.py
@@ -1019,7 +1019,5 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-@app.route("/")
-def home():
-    return render_template("index.html")
+
+    app.run(host="0.0.0.0", port=10000)
